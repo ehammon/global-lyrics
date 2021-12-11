d0d4865a9aab0cfa92d9f85a96ed214d8d5b5e39
Emily Hammon <ehammon7@gmail.com>
2021-12-10 19:35:26 -0500

    added input and button and genius api shows search results in console

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,16 @@
+const clickSearch = function() {
+	let inputValue = $("#songName").val()
+	console.log(inputValue);
+	getLyrics(inputValue);
+}
+
 //genius API
+
+const getLyrics = function(songTitle) {
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://genius.p.rapidapi.com/artists/16775/songs",
+	"url": `https://genius.p.rapidapi.com/search?q=${songTitle}`,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "genius.p.rapidapi.com",
@@ -10,29 +18,28 @@ const settings = {
 	}
 };
 
+
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
+}
+
+$("#search-button").on("click", clickSearch);
 
 // google translate 'translate'
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
-	"method": "POST",
-	"headers": {
-		"content-type": "application/x-www-form-urlencoded",
-		"accept-encoding": "application/gzip",
-		"x-rapidapi-host": "google-translate1.p.rapidapi.com",
-		"x-rapidapi-key": "2da096b016msh4eb9721ad5715a3p16841cjsnfd3b68cc2483"
-	},
-	"data": {
-		"q": "Hello, world!",
-		"target": "es",
-		"source": "en"
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+const translateSettings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://google-translate1.p.rapidapi.com/language/translate/v2/languages",
+		"method": "GET",
+		"headers": {
+			"accept-encoding": "application/gzip",
+			"x-rapidapi-host": "google-translate1.p.rapidapi.com",
+			"x-rapidapi-key": "2da096b016msh4eb9721ad5715a3p16841cjsnfd3b68cc2483"
+		}
+	};
+	
+	$.ajax(translateSettings).done(function (response) {
+		console.log(response);
+	});
+	
