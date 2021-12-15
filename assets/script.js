600b5039cb2f32ecbf283bb25396f57702f2ca00
Cody Webb <webbcody36@gmail.com>
2021-12-15 16:20:37 -0500

    local storage

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
+
+
+
 const clickSearch = function() {
 	let inputValue = $("#songName").val()
-	console.log(inputValue);
-	getLyrics(inputValue);
+	window.localStorage.setItem('Song', inputValue, JSON.stringify());
+	for(var i = 0; i < inputValue.length; i++){
+		inputValue.push(inputValue[i]);
+	}
+	localStorage.setItem("Song", JSON.stringify(inputValue));
 }
 
 //genius API
@@ -48,29 +54,6 @@ const translateSettings = {
 $.ajax(translateSettings).done(function (response) {
 	console.log(response);
 });
-	
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
